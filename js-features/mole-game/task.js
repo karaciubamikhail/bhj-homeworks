a6fbb6502b40f5e939cc100a52707ff2400a85aa
Mikhail <134963272+karaciubamikhail@users.noreply.github.com>
2023-11-24 17:27:20 +0300

    Add files via upload

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,28 @@
+let holes = document.querySelectorAll(".hole");
+let dead = document.querySelector("#dead");
+let lost=document.querySelector("#lost");
+let countDead=0;
+let countLost=0;
+
+for(let i=0;i<holes.length;i++){
+    holes[i].addEventListener("click",(e)=>{
+       let element = e.target;
+       if(element.classList.contains("hole_has-mole")){
+        countDead+=1;
+        dead.innerHTML=countDead;
+        if(countDead===10){
+            countDead=0;
+            countLost=0;
+            alert("Победа");
+        }
+       }else{
+        countLost+=1;
+        lost.innerHTML=countLost;
+        if(countLost===5){
+            countDead=0;
+            countLost=0;
+            alert("Поражение");
+        }
+       }
+    })
+}
