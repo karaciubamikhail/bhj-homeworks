033f74d0aab1b32fd5994eb7f59590b6369cdbe3
Mikhail <134963272+karaciubamikhail@users.noreply.github.com>
2023-12-18 11:16:16 +0300

    Add files via upload

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,10 @@
 let time = document.querySelector(".timer").textContent;
-Number(time);
 time=5;
 setInterval(function(){
     time=time-1;
     document.querySelector(".timer").textContent=time;
     if(time===0){
         alert("Вы победили")
-        return;
+        clearInterval();
     }
 },1000)
